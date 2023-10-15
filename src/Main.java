6c8444f47ca9708f0f21e145a8069d36aa6c19f1
Eduard-Prinz <edwardprince1112@gmail.com>
2023-10-15 13:39:01 +0300

    Начало выполнения домашнего задания №10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника - " + fullName);
+
+
+        String uppercaseFullName = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета - " + uppercaseFullName);
+
+
+        String firstName1 = "Семён";
+        String middleName1 = "Семёнович";
+        String lastName1 = "Иванов";
+        String fullName1 = lastName1 + " " + firstName1 + " " + middleName1;
+        fullName1 = fullName1.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника - " + fullName1);
     }
 }
